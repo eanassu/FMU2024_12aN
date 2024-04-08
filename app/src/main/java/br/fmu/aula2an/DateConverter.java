394ca2a50e55ae123285efa42f5462ffbb8e109a
eanassu <eanassu@gmail.com>
2024-04-08 20:55:19 -0300

    Aula 08/4

---
+++ app/src/main/java/br/fmu/aula2an/DateConverter.java
@@ -0,0 +1,14 @@
+package br.fmu.aula2an;
+
+import androidx.room.TypeConverter;
+import java.util.Date;
+public class DateConverter {
+    @TypeConverter
+    public static Date toDate(Long timestamp) {
+        return timestamp==null?null:new Date(timestamp);
+    }
+    @TypeConverter
+    public static Long toTimestamp(Date date) {
+        return date==null?null: date.getTime();
+    }
+}
