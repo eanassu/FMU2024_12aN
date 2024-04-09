0e9612023dbd15fcaa869124a431316458404def
eanassu <eanassu@gmail.com>
2024-04-08 21:45:28 -0300

    Aula 08/4

---
+++ app/src/main/java/br/fmu/aula2an/DatabaseActivity.java
@@ -0,0 +1,20 @@
+package br.fmu.aula2an;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        Funcionario f = new Funcionario(1, "teste1", new Date(), 1000);
+        dao.insert(f);
+    }
+}
